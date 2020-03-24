2573fc4b82267ec6b5e96752d1744dbfc1334f03
Micah <micah_schiewe1@baylor.edu>
2020-03-24 16:09:18 -0500

    Fix failing test
    * Issue: exception thrown from finally {} overshadowing other exceptions

--- src/main/java/edu/baylor/ecs/si/Timer.java
+++ src/main/java/edu/baylor/ecs/si/Timer.java
@@ -6,43 +6,47 @@ import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Timer {
-	
+
 	private static Logger logger = Logger.getLogger(Timer.class.getName());
-	
+
 	static {
-		try {
-			InputStream configFile = Timer.class.getClassLoader().getResourceAsStream("logger.properties");
+		// Note to writer: do not rely on a try/catch block running to completion to
+		// close a resource! It may leak a file handle!
+		try (InputStream configFile = Timer.class.getClassLoader().getResourceAsStream("logger.properties")) {
 			LogManager.getLogManager().readConfiguration(configFile);
-			configFile.close();
 		} catch (IOException ex) {
 			System.out.println("WARNING: Could not open configuration file");
-		    System.out.println("WARNING: Logging not configured (console output only)");
+			System.out.println("WARNING: Logging not configured (console output only)");
 		}
 		logger.info("starting the app");
 	}
-	
+
 	/*
 	 * Just sleep
 	 */
 	private static void method(long time) throws InterruptedException {
 		Thread.sleep(time);
 	}
-	
+
 	public static long timeMe(long timeToWait) throws TimerException {
 		Long timeNow = null;
 		try {
-		  if (timeToWait < 0) {
-			  throw new TimerException("Cannot be less than zero");
-		  }
-	      timeNow = System.currentTimeMillis();
-		  method(timeToWait);
-			
+			if (timeToWait < 0) {
+				throw new TimerException("Cannot be less than zero");
+			}
+			timeNow = System.currentTimeMillis();
+			method(timeToWait);
+
 		} catch (InterruptedException e) {
 			logger.severe("InterruptedException rised");
 			throw new TimerException("Sleep exception", e);
 		} finally {
-			logger.info("Calling took: "+ (System.currentTimeMillis() - timeNow));
-			logger.info("* should take: "+ timeToWait);
+			if (timeNow != null) {
+				logger.info("Calling took: " + (System.currentTimeMillis() - timeNow));
+				logger.info("* should take: " + timeToWait);
+			} else {
+				logger.warning("Call did not begin properly");
+			}
 		}
 		return timeNow;
 	}
